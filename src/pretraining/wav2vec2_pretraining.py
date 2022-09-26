19adee790d157a1a2f881cf2688336d1dd876260
ujan <ujandev@gmail.com>
2022-09-26 15:08:37 +0530

    data collator

--- src/pretraining/wav2vec2_pretraining.py
+++ src/pretraining/wav2vec2_pretraining.py
@@ -7,7 +7,8 @@ import torch.nn as nn
 import torch.distributed as dist
 from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.cuda import amp
-from transformers import SchedulerType, set_seed, is_wandb_available
+from transformers import SchedulerType, set_seed, is_wandb_available, Wav2Vec2Config
+from datasets import load_from_disk
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Pretrain a Wav2Vec2 model")
@@ -103,7 +104,7 @@ def parse_args():
     parser.add_argument(
         "--pad_to_multiple_of",
         type=int,
-        default=None,
+        default=32,
         help=(
             "If set will pad the sequence to a multiple of the provided value. This is especially useful to enable the"
             " use of Tensor Cores on NVIDIA hardware with compute capability >= 7.5 (Volta)."
@@ -139,6 +140,84 @@ def parse_args():
     return args
 
 
+@dataclass
+class DataCollatorForWav2Vec2Pretraining:
+    """
+    Data collator that will dynamically pad the inputs received and prepare masked indices
+    for self-supervised pretraining.
+    Args:
+        model (:class:`~transformers.Wav2Vec2ForPreTraining`):
+            The Wav2Vec2 model used for pretraining. The data collator needs to have access
+            to config and ``_get_feat_extract_output_lengths`` function for correct padding.
+        feature_extractor (:class:`~transformers.Wav2Vec2FeatureExtractor`):
+            The processor used for proccessing the data.
+        padding (:obj:`bool`, :obj:`str` or :class:`~transformers.tokenization_utils_base.PaddingStrategy`, `optional`, defaults to :obj:`True`):
+            Select a strategy to pad the returned sequences (according to the model's padding side and padding index)
+            among:
+            * :obj:`True` or :obj:`'longest'`: Pad to the longest sequence in the batch (or no padding if only a single
+              sequence if provided).
+            * :obj:`'max_length'`: Pad to a maximum length specified with the argument :obj:`max_length` or to the
+              maximum acceptable input length for the model if that argument is not provided.
+            * :obj:`False` or :obj:`'do_not_pad'` (default): No padding (i.e., can output a batch with sequences of
+              different lengths).
+        max_length (:obj:`int`, `optional`):
+            Maximum length of the ``input_values`` of the returned list and optionally padding length (see above).
+        pad_to_multiple_of (:obj:`int`, `optional`):
+            If set will pad the sequence to a multiple of the provided value.
+            This is especially useful to enable the use of Tensor Cores on NVIDIA hardware with compute capability >=
+            7.5 (Volta).
+    """
+
+    model: Wav2Vec2ForPreTraining
+    feature_extractor: Wav2Vec2FeatureExtractor
+    padding: Union[bool, str] = "longest"
+    pad_to_multiple_of: Optional[int] = None
+
+    def __call__(self, features: List[Dict[str, Union[List[int], torch.Tensor]]]) -> Dict[str, torch.Tensor]:
+        # reformat list to dict and set to pytorch format
+        batch = self.feature_extractor.pad(
+            features,
+            padding=self.padding,
+            pad_to_multiple_of=self.pad_to_multiple_of,
+            return_tensors="pt",
+        )
+
+        device = batch["input_values"].device
+        batch_size = batch["input_values"].shape[0]
+
+        mask_indices_seq_length = self.model._get_feat_extract_output_lengths(batch["input_values"].shape[-1])
+        # make sure masked sequence length is a Python scalar
+        mask_indices_seq_length = int(mask_indices_seq_length)
+
+        # make sure that no loss is computed on padded inputs
+        if batch.get("attention_mask") is not None:
+            # compute real output lengths according to convolution formula
+            batch["sub_attention_mask"] = self.model._get_feature_vector_attention_mask(
+                mask_indices_seq_length, batch["attention_mask"]
+            )
+
+        features_shape = (batch_size, mask_indices_seq_length)
+
+        # sample randomly masked indices
+        mask_time_indices = _compute_mask_indices(
+            features_shape,
+            self.model.config.mask_time_prob,
+            self.model.config.mask_time_length,
+            attention_mask=batch.get("sub_attention_mask"),
+        )
+
+        # sample negative indices
+        sampled_negative_indices = _sample_negative_indices(
+            features_shape,
+            self.model.config.num_negatives,
+            mask_time_indices=mask_time_indices,
+        )
+        batch["mask_time_indices"] = torch.tensor(mask_time_indices, dtype=torch.long, device=device)
+        batch["sampled_negative_indices"] = torch.tensor(sampled_negative_indices, dtype=torch.long, device=device)
+
+        return batch
+
+
 def main():
 
     args = parse_args()
@@ -154,6 +233,36 @@ def main():
     # seed
     if args.seed is not None:
         set_seed(args.seed)
+
+    # load vectorized dataset
+    vectorized_datasets = load_from_disk(args.dataset)
+
+    # config
+
+    # huggingface pretraining script only supports "newer" stable layer norm architecture
+    # apply_spec_augment has to be True, mask_feature_prob has to be 0.0
+
+    # feat_extract_norm (str, optional, defaults to "group") — the norm to be applied to 1D convolutional layers in feature encoder
+    # one of "group" for group normalization of only the first 1D convolutional layer or
+    # "layer" for layer normalization of all 1D convolutional layers.
+
+    # apply_spec_augment (bool, optional, defaults to True) — Whether to apply SpecAugment data augmentation to the outputs of the feature encoder
+    # for reference see SpecAugment: A Simple Data Augmentation Method for Automatic Speech Recognition
+
+    # mask_feature_prob (float, optional, defaults to 0.0) — Percentage (between 0 and 1) of all feature vectors along the feature axis which will be masked
+    # the masking procecure generates ”mask_feature_problen(feature_axis)/mask_time_length” independent masks over the axis
+    # if reasoning from the propability of each feature vector to be chosen as the start of the vector span to be masked,
+    # mask_feature_prob should be `prob_vector_startmask_feature_length
+    # note that overlap may decrease the actual percentage of masked vectors
+    # this is only relevant if apply_spec_augment is True`
+
+    config = Wav2Vec2Config(feat_extract_norm='layer')
+    print(config)
+
+if __name__ == "__main__":
+  main()
+
+
 
 
 
