eval flax model on full data, pytorch weights. flax cer (attention mask). pruning benchmarks. jax multithreading error in map. replicate bug in orbax. restore when checkpoint structures differ. accelerate delete older checkpoints. quantization. eval working. train + distill, quant, prune on cloud. dynamic quant. quant support on 8 core. trace whisper. efficient attention?

src/finetuning/whisper/pytorch/whisper_cloud_eval.py
@@ -10,7 +10,6 @@
 
 """
 
-from functools import partial
 import os, re
 import datetime
 from os.path import dirname, abspath
@@ -28,7 +27,7 @@ from typing import Any, Dict, List, Union
 import evaluate
 from transformers import AutoModelForSpeechSeq2Seq
 from torch.utils.data.dataloader import DataLoader
-from transformers import AdamW, get_linear_schedule_with_warmup, set_seed
+from transformers import set_seed
 import argparse
 from accelerate import Accelerator
 