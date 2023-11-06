5cde779533247306a3acb8bf021f8495cc142f82
ujan <ujandev@gmail.com>
2023-11-06 18:52:56 +0530

    debug static quant. moe, sparse act, act pathways. prune conv. split reg for cofi prune? graph message passing, local smoothing, reverse diffusion, distribution uniformity. add masking in modeling_whisper. sparsewhisperencoderlayer, hidden_z in layernorm, embeddings. eval, predict. add l1, l2 reg. group masks?

--- src/cofi_prune/modeling_whisper.py
+++ src/cofi_prune/modeling_whisper.py
@@ -324,7 +324,6 @@ class SparseLayerNorm(torch.nn.LayerNorm):
             output = F.layer_norm(
                 input, self.normalized_shape, self.weight, self.bias, self.eps)
         return output
-    
 
 
 class WhisperPositionalEmbedding(nn.Embedding):
@@ -333,7 +332,6 @@ class WhisperPositionalEmbedding(nn.Embedding):
 
     def forward(self, input_ids, past_key_values_length=0):
         return self.weight[past_key_values_length : past_key_values_length + input_ids.shape[1]]
-
 
 
 ## fix this ##
